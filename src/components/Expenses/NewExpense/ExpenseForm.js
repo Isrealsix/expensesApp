a2ab41f0d365cd3037c25588437c19898cf4b9ed
Isreal6ix <isrealsix@gmail.com>
2021-07-25 00:56:24 +0700

    added child to parent component communication

--- src/components/Expenses/NewExpense/ExpenseForm.js
+++ src/components/Expenses/NewExpense/ExpenseForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './ExpenseForm.css';
-const ExpenseForm = () => {
+
+const ExpenseForm = props => {
 	const [enteredTitle, setEnteredTitle] = useState('');
 	const [enteredAmount, setEnteredAmount] = useState('');
 	const [enteredDate, setEnteredDate] = useState('');
@@ -44,6 +45,7 @@ const ExpenseForm = () => {
 		};
 
 		console.log(expensesData);
+		props.onSaveExpenseData(expensesData);
 		setEnteredTitle('');
 		setEnteredDate('');
 		setEnteredAmount('');

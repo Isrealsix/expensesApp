71bb35e43f1fe3ffb09fadbca7a372914c3a78e3
Isreal6ix <isrealsix@gmail.com>
2021-07-24 22:09:37 +0700

    added useState to maintain state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,25 @@
+import React, { useState } from 'react';
+
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 const ExpenseItem = props => {
+	const [title, setTitle] = useState(props.title);
+
+	const clickHandler = () => {
+		console.log('clicked', title);
+		setTitle('Updated!');
+	};
+
 	return (
 		<Card className="expense-item">
 			<div className="expense-item__description">
 				<ExpenseDate date={props.date} />
-				<h2>{props.title}</h2>
+				<h2>{title}</h2>
 				<div className="expense-item__price">${props.amount}</div>
 			</div>
+			<button onClick={clickHandler}>clickist</button>
 		</Card>
 	);
 };

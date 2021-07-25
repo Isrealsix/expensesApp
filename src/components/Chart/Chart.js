3592021cb3198c4320a331dd4bac5bda42dcd1ea
Isreal6ix <isrealsix@gmail.com>
2021-07-26 02:48:40 +0700

    added the Chart component and added dynamic styles to the bar

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Chart.css';
+import ChartBar from './ChartBar';
+
+const Chart = props => {
+	return (
+		<div className="chart">
+			{props.dataPoints.map(dataPoint => (
+				<ChartBar
+					id={dataPoint.value}
+					value={dataPoint.value}
+					maxValue={null}
+					label={dataPoint.label}
+				/>
+			))}
+		</div>
+	);
+};
+
+export default Chart;
